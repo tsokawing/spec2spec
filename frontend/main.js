ba127328756a98937b32524ee330f61de8d5d4da
Colin Tso <tsokawing@protonmail.com>
2022-04-03 01:26:47 +0800

    Create frontend

---
+++ frontend/main.js
@@ -0,0 +1,39 @@
+const inputAudio = document.getElementsByTagName("audio")[0];
+const predAudio = document.getElementsByTagName("audio")[1];
+
+const input = document.getElementById("upload");
+
+// To fire onchange event even if the chosen file name does not change.
+input.onclick = function () {
+  this.value = "";
+};
+
+input.addEventListener("change", (e) => {
+  // Change audio player source to the chosen file.
+  const file = e.target.files[0];
+  inputAudio.src = URL.createObjectURL(file);
+
+  // Send POST request to server for prediction.
+  sendPredictRequest(file);
+});
+
+function sendPredictRequest(file) {
+  console.log("processing");
+  const predictURL = "http://127.0.0.1:5000/predict";
+  const formData = new FormData();
+  formData.append("audio_data", file);
+
+  fetch(predictURL, {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => {
+      console.log(res.body);
+      return res.body;
+    })
+    .then((stream) => new Response(stream))
+    .then((response) => response.blob())
+    .then((blob) => URL.createObjectURL(blob))
+    .then((url) => console.log((predAudio.src = url)))
+    .catch((err) => console.error(err));
+}

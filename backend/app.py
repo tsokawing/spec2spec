a5d0949198764000e37d8c1c25b3074c880ea9cd
Vincent Wong <vincentwwy168@gmail.com>
2022-04-14 00:14:20 +0800

    Procedure to remove old files

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, send_file
 from infer import Spec2spec, load_audio, save_audio, to_melspectrogram, save_melspectrogram
+import datetime
 
 UPLOAD_FOLDER = 'uploads'
 OUTPUT_FOLDER = 'output'
@@ -11,10 +12,43 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 spec2spec = Spec2spec()
 
+# files are allowed max 15 minutes of existence in our server
+app.config['FILE_TIMEOUT_SECONDS'] = 15 * 60
+
 
 def is_allowed(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
+def remove_old_files():
+    proj_root_dir = os.path.dirname(__file__)
+    now_time = datetime.datetime.now()
+    # deletes old files that are too old; both uploads and outputs
+    for dir_name in ['uploads', 'output']:
+        full_dir_name = proj_root_dir + '/' + dir_name
+        for file_name in os.listdir(full_dir_name):
+            if not os.path.isfile(full_dir_name + '/' + file_name):
+                # not a file
+                continue
+            # is a file, check whether it matches
+            base_name = os.path.split(file_name)[1]
+            need_delete = False
+            # it is always eg upload_2022_04_01_12_34_56.wav
+            # in general, [type]_[timestamp].[filetype]
+            try:
+                components = [int(x) for x in base_name.split('.')[0].split('_')[1:7]]
+                file_stamp = datetime.datetime(components[0], components[1], components[2], components[3],
+                                               components[4], components[5])
+                age = now_time - file_stamp
+                if age.total_seconds() <= app.config['FILE_TIMEOUT_SECONDS']:
+                    # not old enough
+                    continue
+                # delete the file!
+                os.remove(full_dir_name + '/' + file_name)
+            except Exception:
+                # does not match
+                continue
+    pass
 
 @app.route('/predict', methods=['POST'])
 def predict():
